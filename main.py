64775bbb608dcc150b8604e43b9a5e800a2a84ba
Mehak-B18068 <b18068@students.iitmandi.ac.in>
2020-10-22 13:30:22 +0530

    Modified GUI

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import PySimpleGUI as sg 
+import nltk
 import os.path
-	
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+
 # sg.theme('BrightColors')
 sg.theme('Topanga')
 
@@ -19,21 +23,21 @@ file_list_column = [
 
     [
 
-        sg.Listbox(
+        sg.Multiline(
 
-            values=['Sentences: __Words: __'], enable_events=True, size=(30, 10), key="-FILE CONTENT-"
+              size=(50, 5), key="-FILE CONTENT-"
 
         )
 
     ],
     [
-        sg.Listbox(values=["__Histogram__"], size=(30, 10), key='histogram')
+        sg.Canvas(size=(30, 5), key='-CANVAS-')
     ],
     [
         sg.Button("KeyWords")
     ],
     [
-        sg.Listbox(values=[" __Sentences with keywords__"], size=(30,10), key='keywords')
+        sg.Multiline(size=(50,5), key='keywords')
     ],
 
 ]
@@ -48,19 +52,35 @@ layout = [
 
 ]   
 
+def draw_figure(canvas, figure, loc=(0, 0)):
+    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+    return figure_canvas_agg
 
 window = sg.Window('Introduction_LAP_LAB3', layout) 
 
 while True: 
-	event, values = window.read() 
-	print(event, values) 
-	
-	if event in (None, 'Exit'): 
-		break
-	
-	
-	if event == 'Edit': 
-		# Update the "output" text element 
-		# to be the value of "input" element 
-		window['-OUTPUT-'].update(values['xx']) 
+    event, values = window.read() 
+    #print(event, values) 
+    
+    if event in (None, 'Exit'): 
+        break
+    
+    if event == 'Edit': 
+        #plt.plot([0.1, 0.2, 0.5, 0.7])
+        processfile(values['-FOLDER-'])
+        fig=plt.gcf()
+        fig_photo = draw_figure(window['-CANVAS-'].TKCanvas, fig)
+        #window['-FILE CONTENT-'].print(values['-FOLDER-'])
+        #window['-FILE CONTENT-'].print("Bye")
+        
+        #print(values['-FOLDER-'])
+        
+        #Update the "output" text element 
+        # to be the value of "input" element 
+        
+       # window['-OUTPUT-'].update(values['xx']) 
+
 window.close() 
+    
