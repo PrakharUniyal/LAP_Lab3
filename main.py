092bb5747fd1fa5df774324eba7f86cf99bcb93d
Mehak-B18068 <b18068@students.iitmandi.ac.in>
2020-10-22 13:55:53 +0530

    Added Statistics and Histogram

--- main.py
+++ main.py
@@ -4,7 +4,34 @@ import os.path
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-
+def processfile(textfile):
+    
+#from nltk.stem import WordNetLemmatizer
+#from nltk.probability import FreqDist
+    
+    
+    f = open(textfile, encoding="utf8")
+    ftext = f.read()
+    nwln=ftext.split('\n')
+    # If you would like to work with the novel in nltk.Text format you can use 'text1'
+    tokens = nltk.word_tokenize(ftext)
+    sentences=nltk.sent_tokenize(ftext)
+    window['-FILE CONTENT-'].print("No. of words:",len(tokens))
+    window['-FILE CONTENT-'].print("No. of sentences:",len(sentences))
+    window['-FILE CONTENT-'].print("No. of newlines:",len(nwln))
+    #text1 = nltk.Text(ftext)
+    sw=nltk.corpus.stopwords.words('english')
+    lemma=nltk.WordNetLemmatizer()
+    text=[lemma.lemmatize(w.lower()) for w in tokens if len(w)>3]
+    dist=nltk.FreqDist([word for word in text if word not in sw])
+    dic=dict(dist)
+    sorteddic=sorted(dic.items(), key = lambda ele: ele[1], reverse = True)[:20]
+    window['-FILE CONTENT-'].print("Most frequent word:",sorteddic[0][0])
+    
+    f=[[x,y] for x,y in sorteddic]
+    plt.bar([x[0] for x in f],[x[1] for x in f])
+    plt.xticks(rotation=45)
+    
 # sg.theme('BrightColors')
 sg.theme('Topanga')
 
